0c76ea1c951004f3b4a16cb6bbfa972df4601dca
ErsinGabbas <ersingabbas1315@gmail.com>
2023-04-04 21:15:01 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    //Function
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    //Function work check
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = fibonacci(n);
+        System.out.println(result);
+    }
+}
